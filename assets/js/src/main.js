62eea9848c34799bb0d39111ac5a36fb46a1d6df
Cipscis <cipscis+github@gmail.com>
2021-06-19 02:49:32 +0000

    Deploying to gh-pages from @ Cipscis/codebook@a765109393600645b4934fd8cbbf05c996029fcb 🚀

---
+++ assets/js/src/main.js
@@ -0,0 +1,12 @@
+// Using import from '/filename', Webpack will
+// bundle files from outside the docs directory
+// even though it is the root for the server
+// both locally and on GitHub Pages
+
+import codebook from '/codebook.js';
+
+codebook.tidy();
+
+codebook.run({
+	testArg: 'test',
+});
